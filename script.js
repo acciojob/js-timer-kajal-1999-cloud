49fef0264bb7df6d20a14add11a2a18de96ed118
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-10-21 16:32:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 // write js code here if required
+var timer = document.getElementById("timer");
+function currentTime() {
+  let date = new Date(); 
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let currentDate = `${month}/${day}/${year}`;
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+  let session = "AM";
+
+  if(hh == 0){
+      hh = 12;
+  }
+  if(hh > 12){
+      hh = hh - 12;
+      session = "PM";
+   }
+
+   hh = (hh < 10) ? "0" + hh : hh;
+   mm = (mm < 10) ? "0" + mm : mm;
+   ss = (ss < 10) ? "0" + ss : ss;
+    
+   let time = currentDate + ", " + hh + ":" + mm + ":" + ss + " " + session;
+
+  timer.innerText = time; 
+  let t = setTimeout(function(){ currentTime() }, 1000);
+}
+currentTime();
